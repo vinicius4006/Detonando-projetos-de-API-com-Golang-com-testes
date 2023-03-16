219475b692911719783fbe1d9bb9eef390285e45
Vinicius <vfranca477@gmail.com>
2023-03-15 21:15:44 -0300

    refatorado

--- s16-desafio/main.go
+++ s16-desafio/main.go
@@ -4,61 +4,28 @@ import "fmt"
 
 type Solution struct{}
 
-func (s Solution) twoSums(nums []int, target int) []int {
-	dictValues := make(map[int]int)
-
-	for ix, val := range nums {
-		dictValues[val] = ix
-	}
-
-	for ix, val := range nums {
-		complement := target - val
-		if _, ok := dictValues[complement]; ok && dictValues[complement] != ix {
-			return []int{ix, dictValues[complement]}
-		}
-	}
-	return nil
-}
-
 func main() {
 	vetores := []int{2, 7, 11, 15, 4, 9, 4, 5, 78, 21, 65, 87, 98, 21, 5874, 5, 61, 32, 32, 32}
-	somaValor := 67
-	// minha solucacao
-	minhaSolucaoSoma(vetores, somaValor)
+	somaValor := 14
 
-	//solucao professor
-	var s Solution
-	arr := s.twoSums(vetores, somaValor)
-	fmt.Printf("%v %v", arr[0], arr[1])
+	minhaSolucaoSoma(vetores, somaValor)
 
 }
 
 func minhaSolucaoSoma(vetores []int, alvo int) {
-	stopFor := false
-	inicialPositionVetor := 0
-	position := 0
-	newArray := []int{}
-	for {
 
-		var soma int
-		if stopFor || inicialPositionVetor >= len(vetores) {
+	mapVetores := make(map[int]int)
+
+	for idx, value1 := range vetores {
+		mapVetores[value1] = idx
+	}
+	for idx2, value2 := range vetores {
+		complemento := alvo - value2
+		_, ok := mapVetores[complemento]
+		if ok && mapVetores[complemento] != idx2 {
+			fmt.Printf("%v %v", value2, complemento)
 			break
 		}
-		if position < len(vetores) {
-			if position == inicialPositionVetor && inicialPositionVetor < len(vetores)-1 {
-				position++
-			}
-			soma = vetores[inicialPositionVetor] + vetores[position]
-			if soma == alvo {
-				newArray = append(newArray, inicialPositionVetor, position)
-				stopFor = true
-			}
-			position++
-		} else {
-			inicialPositionVetor++
-			position = 0
-		}
-
 	}
-	fmt.Printf("%v", newArray)
+
 }
